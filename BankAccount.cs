a81526d03caa4d9dcf76c49399dacdd74d21c0d9
Grant Hill <granthill@live.com>
2022-09-05 15:10:53 -0400

    first commit.

---
+++ BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EncapsulationIntro
+{
+    internal class BankAccount
+    {
+        private double balance; //by creating a private access modifier, the value of 'balance' is protected from public access.
+
+        public double CheckingAccount(double amount)
+        {
+            return balance;
+        }
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Synthesizing your deposit of {amount, 0:c}"); //weird that the variable, 'amount' is able to emulate the user's input from another variable entirely (depositedAmount).
+            balance = amount; //initializing amount to balance.
+        }
+        public void DepositVariant(double DepositAmount)       //only within the class that is titled 'balance' can balance be accessed.
+        {
+            balance += DepositAmount;
+        }
+        
+        public double GetBalance() //method to be called for when we want to know what the user's balance is. We can do this without error, because 'balance' has already been defined
+        {                          //by the variable, 'amount', and when the method 'Deposit' gets called by the main script, the variable 'amount' is implemented as a means of storing
+            return balance;        //the information taken in by the variable, 'depositedAmount'. Perhaps it's like two different programs talking to eachother -- they call the same
+        }                          //information different things.
+        public void Withdrawal(double WithdrawalAmount) //both methods, 'Deposit', and 'Withdrawal', are written within the class of 'balance'.
+        {                                               //therefore, they can access stored information pertaining to 'balance'.
+            balance -= WithdrawalAmount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account1 = new BankAccount();
+
+            Console.WriteLine("This is an introduction to encapsulation.");
+            Console.WriteLine();
+            Console.WriteLine("We will write a script that stores a user's input, and protects it from being seen by anyone else.");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to your local bank ATM. How much do you wish to deposit today?");
+            Console.WriteLine();
+            double depositedAmount = double.Parse(Console.ReadLine()); //user input gets stored in newly assigned variable.
+            Console.WriteLine();
+            account1.Deposit(depositedAmount); //previously defined script (method) in our other class file gets called, and runs. It is also taking the information
+            double yourBalance = account1.GetBalance(); //from the user and allowing it to be stored on both programs.
+            Console.WriteLine(); //in the above line, we take the stored information from the user and initialize plus assign it to another variable, that also uses (.) notation to call another predefined method which can then be used to print it.
+            Console.WriteLine($"The remaining balance in your account is now {yourBalance, 0:c}"); //now, we want to showcase to the user what their remaining balance is.
+            Console.WriteLine();
+            //account1.Deposit(400);
+
+
+            //account1.Withdrawal(100);
+
+            //account.balance(); if we try to implement the values stored within the class, 'balance', visual studio will tell you that such a class
+        }   //is not 'invocable', or usable, and this is because its access modifier has been set to private.
+    }
+}
